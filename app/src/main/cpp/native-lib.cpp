cc948be849b8e5623b83f6f96fea2637a84d8506
chendilin <1399484526@qq.com>
2021-08-18 17:59:23 +0800

    添加ffmpeg库，修改CMake，正常调用ffmpeg库函数

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,15 @@
 #include <jni.h>
 #include <string>
 
+extern "C" {
+    #include <libavcodec/avcodec.h>
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_miniplayer_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    hello += avcodec_configuration();
     return env->NewStringUTF(hello.c_str());
 }
